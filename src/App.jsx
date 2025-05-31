5203ac67c442b503cb3edcc9019d6494902dcfb4
Chico204 <olatunjimark391@gmail.com>
2025-05-31 01:37:59 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Navbar from './Navbar';
+function App() {
+ return(
+  <div>navbar</div>
+ )
+}
+
+export default App
---
+++ src/Navbar.jsx
@@ -0,0 +1,9 @@
+import React, { useState, useRef, useEffect } from 'react';
+import { FaBars } from 'react-icons/fa';
+import { links, social } from './data';
+
+
+const Navbar =()=>{
+
+}
+export default Navbar
